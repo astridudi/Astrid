adf2a998ca39cc3992c45cd06d6941a67c6d686d
astridudi <astrid@udi.edu.co>
2020-09-17 08:22:26 -0500

    Corrección problema con cadenas JSON
    Se corrige problema presentado con caracteres de cambio de línea en los mensajes del chat

--- public/javascripts/extraccionCadenaJson.js
+++ public/javascripts/extraccionCadenaJson.js
@@ -1,5 +1,6 @@
 function cadenaJson(pCadenaJson) {
     let rCadenaJson = pCadenaJson.replace(/&quot;/g, "'");
+    rCadenaJson = rCadenaJson.replace(/\n|\r/g, ' ');
     rCadenaJson = rCadenaJson.replace(/'/g, '"');
     return(JSON.parse(rCadenaJson));
 }
